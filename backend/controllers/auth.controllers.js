790d77aefa084821ba7cd3d70f7461f08ceb480d
yomayo <abdigali.a20@gmail.com>
2025-02-26 15:35:28 +0500

    update database

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,7 +1,47 @@
+import User from "../models/userModel.js"
+
 export const signup = async(req, res) =>{
     try {
+        console.log(req.body)
         const {fullName, username, password, confirmPassword, gender} = req.body;
+
+        if (password !== confirmPassword){
+            return res.status(400).json({error: "Passwords do not match"})
+        }
+
+        const user = await User.findOne({username})
+
+        if(user) 
+        {
+            return res.status(400).json({error: "Username already exists"})
+        }
+
+        // HASH PASSWORD HERE
+        
+        const boyPictProf = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlPictProf = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const newUser = new User({
+            fullName,
+            username,
+            password,
+            gender,
+            profilePic: gender === "Male"? boyPictProf : girlPictProf
+        })
+
+        await newUser.save()
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic
+        })
+
     } catch (error) {
+        console.log("Error in signup controller", error.message)
+                    
+        res.status(500).json({error: "Server Error"})
         
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,31 @@
 import express from "express";
 import dotenv from "dotenv"
+import cors from "cors"
 import authRoutes from './routes/auth.routes.js';
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.join(__dirname, ".env") });
+
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 dotenv.config();
 
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/api/auth', authRoutes);
+
 app.get("/", (req, res) => {
-    res.send("Hello WORLD")
-})
+    res.send("Hello WORLD");
+  });
+  
+  
 
-
-app.use("/api/auth", authRoutes)
- 
 app.listen(PORT, ()=>{
     connectToMongoDB();
     console.log(`Server listening on ${PORT}`)
